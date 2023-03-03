7fccf3506e947888984758fbc01a7422f3bd0e18
Sergey Donyk <doncarlos@i.ua>
2023-03-04 00:36:51 +0200

    task-two-s

---
+++ src/js/02-timer.js
@@ -0,0 +1,7 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+};
