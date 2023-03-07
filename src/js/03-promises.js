84e28f3b351f37c1466e875932c840b063c6e849
Sergey Donyk <doncarlos@i.ua>
2023-03-08 01:35:39 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,16 +1,17 @@
 import Notiflix from 'notiflix';
 
-// пошук елементів
-const form = document.querySelector('.form');
-const delay = document.querySelector('input[name="delay"]');
-const step = document.querySelector('input[name="step"]');
-const amount = document.querySelector('input[name="amount"]');
-const btnCreatePromise = document.querySelector('button[type="submit"]');
-console.log(form);
+const refs = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('input[name="delay"]'),
+  step: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
+  btnCreatePromise: document.querySelector('button[type="submit"]'),
+};
+// console.log(form);
 // console.log(delay);
 // console.log(step);
 // console.log(amount);
-console.log(btnCreatePromise);
+console.log(refs.btnCreatePromise);
 
 // функція для створення обіцянки
 function createPromise(position, delay) {
@@ -30,14 +31,14 @@ function createPromise(position, delay) {
 }
 
 // додавання слухача події для кнопки
-btnCreatePromise.addEventListener('click', e => {
+refs.btnCreatePromise.addEventListener('click', e => {
   e.preventDefault(); // відміна оновлення сторінки
 
-  let firstDelay = Number(delay.value); // конвертація затримки в ЧИСЛО
-  let delayStep = Number(step.value); // конвертація кроку в ЧИСЛО
+  let firstDelay = Number(refs.delay.value); // конвертація затримки в ЧИСЛО
+  let delayStep = Number(refs.step.value); // конвертація кроку в ЧИСЛО
   console.log(delayStep);
   // цикл для перебору кількості введень
-  for (let i = 0; i < amount.value; i += 1) {
+  for (let i = 0; i < refs.amount.value; i += 1) {
     // Передача: номера промісу, першу затримку, введену користувачем, і крок.
     createPromise(1 + i, firstDelay + i * delayStep)
       .then(({ position, delay }) => {
