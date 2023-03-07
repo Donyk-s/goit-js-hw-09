a098b311770eb9322e009fea7397635271476598
Sergey Donyk <doncarlos@i.ua>
2023-03-08 01:37:44 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -13,7 +13,6 @@ const refs = {
 // console.log(amount);
 console.log(refs.btnCreatePromise);
 
-// функція для створення обіцянки
 function createPromise(position, delay) {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -30,30 +29,26 @@ function createPromise(position, delay) {
   return promise;
 }
 
-// додавання слухача події для кнопки
 refs.btnCreatePromise.addEventListener('click', e => {
-  e.preventDefault(); // відміна оновлення сторінки
+  e.preventDefault();
 
-  let firstDelay = Number(refs.delay.value); // конвертація затримки в ЧИСЛО
-  let delayStep = Number(refs.step.value); // конвертація кроку в ЧИСЛО
+  let firstDelay = Number(refs.delay.value);
+  let delayStep = Number(refs.step.value);
   console.log(delayStep);
   // цикл для перебору кількості введень
   for (let i = 0; i < refs.amount.value; i += 1) {
-    // Передача: номера промісу, першу затримку, введену користувачем, і крок.
     createPromise(1 + i, firstDelay + i * delayStep)
       .then(({ position, delay }) => {
-        // відображення повідомленя-успіху користувачеві з бібліотеки Notiflix
         Notiflix.Notify.success(
           `✅ Fulfilled promise ${position} in ${delay}ms`
         );
       })
       .catch(({ position, delay }) => {
-        // відображення повідомленя-помилки користувачеві з бібліотеки Notiflix
         Notiflix.Notify.failure(
           `❌ Rejected promise ${position} in ${delay}ms`
         );
       });
   }
 
-  form.reset(); // очищення поля форми
+  form.reset();
 });
